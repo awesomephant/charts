03db6c7d6eaa913eaa586f086a9923dcd5cddddc
Max Kohler <hello@maxkohler.com>
2025-04-30 16:48:43 +0200

    Block out narrative report

--- utils/try_cache.py
+++ utils/try_cache.py
@@ -1,10 +1,12 @@
-def try_cache(cache_path: str, fetch_fn):
+def try_cache(cache_path: str, fetch_fn, silent: bool = True):
 	try:
 		with open(cache_path, "r") as f:
-			print(f"Loaded cached data from {cache_path}")
+			if not silent:
+				print(f"Loaded cached data from {cache_path}")
 			return f.read()
 	except FileNotFoundError:	
-		print(f"Cache miss, fetching...")
+		if not silent:
+			print(f"Cache miss, fetching...")
 		text = fetch_fn()
 		with open(cache_path, "w") as f:
 			f.write(text)
